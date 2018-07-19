a8609b673b5647d2f65c0e0003c0d84a92836698
Hashim <hashim_sayyid@rocketmail.com>
2018-07-19 17:37:08 +0800

    fixed interval settings now come from client

--- src/main/java/com/powerapps/monitor/controller/SchedulerController.java
+++ src/main/java/com/powerapps/monitor/controller/SchedulerController.java
@@ -1,9 +1,5 @@
 package com.powerapps.monitor.controller;
 
-import com.powerapps.monitor.config.JsonToHashMap;
-import com.powerapps.monitor.config.JsonWriter;
-import com.powerapps.monitor.util.Utils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,41 +8,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 @Controller
 public class SchedulerController {
 
-    @Value("${app.fixedSchedulerJson}")
-    private String fixedSchedulerJsonPath;
-
-    private final JsonWriter writer;
-    private final Utils util;
-    private final JsonToHashMap jsonToHashMap;
-
-    @Autowired
-    public SchedulerController(JsonWriter writer,
-                               Utils util,
-                               JsonToHashMap jsonToHashMap){
-        this.writer=writer;
-        this.util=util;
-        this.jsonToHashMap=jsonToHashMap;
-    }
+    @Value("${app.fixedSchedulerProp}")
+    private String fixedSchedulerPropPath;
 
     @RequestMapping(value = "/fixedinterval", method = RequestMethod.GET)
-    public String fixedIntervalSettings(Model model) {
-        model.addAttribute("result", this.jsonToHashMap.toHmap(fixedSchedulerJsonPath));
+    public String fixedIntervalSettings(Model model) throws IOException {
+        Properties prop = new Properties();
+        prop.load(new FileInputStream(fixedSchedulerPropPath));
+        model.addAttribute("result", prop.getProperty("interval"));
         return "fixedIntervalSettings";
     }
 
     @ResponseBody
     @PostMapping("/fixedinterval")
-    public void saveSetting(String interval) {
-        Map<String, String> fixedSetting = new HashMap<>();
-        fixedSetting.put("interval", interval);
-        String jsonOut = writer.generateJson(fixedSetting);
-        util.writeTextFile(fixedSchedulerJsonPath, jsonOut, false);
+    public void saveFixedSetting(String interval) throws IOException {
+        Properties prop = new Properties();
+        prop.load(new FileInputStream(fixedSchedulerPropPath));
+        prop.remove("interval");
+        prop.setProperty("interval", interval);
+        prop.store(new FileOutputStream(fixedSchedulerPropPath), null);
     }
 
     @RequestMapping(value = "/emailscheduler", method = RequestMethod.GET)
--- src/main/java/com/powerapps/monitor/tasks/Scheduler.java
+++ src/main/java/com/powerapps/monitor/tasks/Scheduler.java
@@ -1,39 +1,41 @@
 package com.powerapps.monitor.tasks;
 
-import com.powerapps.monitor.config.JsonToHashMap;
 import com.powerapps.monitor.service.BatchManagerLogService;
 import com.powerapps.monitor.service.EmailClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+/**
+ * This class runs the core scheduling tasks. It takes in the time values from the properties
+ * file and runs a scheduled task.
+ *
+ * @author hashim
+ */
 @Component
+@PropertySource(value = "file:config/fixedSchedulerConfig.properties")
 public class Scheduler {
     /*Required services*/
     private final BatchManagerLogService bmService;
     private final EmailClient emailClient;
-    private final JsonToHashMap jsonToHashMap;
     /*Required variables*/
-    @Value("${app.fixedSchedulerJson}")
-    private String fixedSchedulerJsonPath;
     private static final Logger LOG = LoggerFactory.getLogger(Scheduler.class);
     /*The constructor for the class to inject the necessary services*/
     @Autowired
     public Scheduler(BatchManagerLogService bmService,
-                     EmailClient emailClient,
-                     JsonToHashMap jsonToHashMap){
+                     EmailClient emailClient){
         this.bmService=bmService;
         this.emailClient=emailClient;
-        this.jsonToHashMap=jsonToHashMap;
     }
 
-    @Scheduled(fixedRate = 120000L)
+    @Scheduled(fixedRateString = "${interval}")
     public void runFixedSchedule() throws IOException {
+        LOG.info("We are here fam...");
         bmService.emailAndPersistToCache();
     }
 }

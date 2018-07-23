202d3e21efc39667c06cfb04b091387da748f990
Hashim <hashim_sayyid@rocketmail.com>
2018-07-23 17:47:01 +0800

    Refactored methods out to PropertyFileUtils, completed email scheduler

--- src/main/java/com/powerapps/monitor/config/PropertyFileUtils.java
+++ src/main/java/com/powerapps/monitor/config/PropertyFileUtils.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Properties;
 
 /**
@@ -29,4 +30,21 @@ public class PropertyFileUtils {
         prop.store(new FileOutputStream(path), null);
     }
 
+    public void writeManyPropToFile(String path, HashMap<String, String> keyValuePair)
+            throws IOException{
+        Properties prop = new Properties();
+        prop.load(new FileInputStream(path));
+        for (String key : keyValuePair.keySet())
+            prop.setProperty(key, keyValuePair.get(key));
+        prop.store(new FileOutputStream(path), null);
+    }
+
+    public HashMap<String, String> readManyPropFromFile(String path) throws IOException{
+        HashMap<String, String> currentSettings = new HashMap<>();
+        Properties prop = this.readPropFile(path);
+        for (Object property : prop.keySet()){
+            currentSettings.put(property.toString(), String.valueOf(prop.get(property)));
+        }
+        return currentSettings;
+    }
 }
--- src/main/java/com/powerapps/monitor/controller/SchedulerController.java
+++ src/main/java/com/powerapps/monitor/controller/SchedulerController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 @Controller
 public class SchedulerController {
@@ -17,10 +18,13 @@ public class SchedulerController {
     @Value("${app.fixedSchedulerProp}")
     private String fixedSchedulerPropPath;
 
+    @Value("${app.emailSchedulerProp}")
+    private String emailSchedulerPropPath;
+
     private PropertyFileUtils propertyFileUtils;
 
-    public SchedulerController(PropertyFileUtils propertyFileUtils){
-        this.propertyFileUtils=propertyFileUtils;
+    public SchedulerController(PropertyFileUtils propertyFileUtils) {
+        this.propertyFileUtils = propertyFileUtils;
     }
 
     @RequestMapping(value = "/fixedinterval", method = RequestMethod.GET)
@@ -39,8 +43,25 @@ public class SchedulerController {
     }
 
     @RequestMapping(value = "/emailscheduler", method = RequestMethod.GET)
-    public String emailScheduler(Model model) {
+    public String emailScheduler(Model model) throws IOException {
+        model.addAttribute("result", this.propertyFileUtils.readManyPropFromFile(emailSchedulerPropPath));
+        System.out.println(this.propertyFileUtils.readManyPropFromFile(emailSchedulerPropPath));
         return "emailSchedulerSettings";
+    }
+
+    @ResponseBody
+    @PostMapping("/emailscheduler")
+    public void saveEmailSetting(String year, String month, String day,
+                                 String hour, String minute, String second)
+            throws IOException {
+        HashMap<String, String> keyValuePair = new HashMap<>();
+        keyValuePair.put("year", year);
+        keyValuePair.put("month", month);
+        keyValuePair.put("day", day);
+        keyValuePair.put("hour", hour);
+        keyValuePair.put("minute", minute);
+        keyValuePair.put("second", second);
+        this.propertyFileUtils.writeManyPropToFile(emailSchedulerPropPath, keyValuePair);
     }
 
 }

9b8c467626ca52749b133027c2be264c8581cad4
Hashim <hashim_sayyid@rocketmail.com>
2018-08-08 16:53:03 +0800

    refactored and separated diff reports

--- src/main/java/com/powerapps/monitor/controller/NotificationReportController.java
+++ src/main/java/com/powerapps/monitor/controller/NotificationReportController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -35,6 +36,11 @@ public class NotificationReportController {
     @RequestMapping(value = "/bmemailnotifreport", method = RequestMethod.GET)
     public String batchManagerEmailNotifReport(Model model) {
         return "batchManagerEmailNotifReport";
+    }
+
+    @GetMapping("/adhocemailnotifreport")
+    public String adhocEmailNotifReport(Model model) {
+        return "adhocEmailNotifReport";
     }
 
 }

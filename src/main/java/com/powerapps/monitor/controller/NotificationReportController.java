698f6053498c3f201dfedeaab4c3577c7958c6cd
Hashim <hashim_sayyid@rocketmail.com>
2018-08-09 08:26:15 +0800

    implementation to view logs on front complete

--- src/main/java/com/powerapps/monitor/controller/NotificationReportController.java
+++ src/main/java/com/powerapps/monitor/controller/NotificationReportController.java
@@ -1,7 +1,6 @@
 package com.powerapps.monitor.controller;
 
-import com.powerapps.monitor.util.JsonToHashMap;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.kollect.etl.util.CsvReadWriteUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,35 +10,38 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class NotificationReportController {
-    @Value("${app.bmJson}")
-    private String bmJsonPath;
-    @Value("${app.seJson}")
-    private String seJsonPath;
+    @Value("${app.seAutoEmailLog}")
+    private String seAutoEmailLog;
+    @Value("${app.dcAutoEmailLog}")
+    private String dcAutoEmailLog;
+    @Value("${app.bmAutoEmailLog}")
+    private String bmAutoEmailLog;
+    @Value("${app.adhocEmailLog}")
+    private String adhocEmailLog;
 
-    private JsonToHashMap jsonToHashMap;
-
-    @Autowired
-    public NotificationReportController(JsonToHashMap jsonToHashMap){
-        this.jsonToHashMap=jsonToHashMap;
-    }
+    private final CsvReadWriteUtils csvReadWriteUtils = new CsvReadWriteUtils(new com.kollect.etl.util.Utils());
 
     @RequestMapping(value = "/seemailnotifreport", method = RequestMethod.GET)
     public String serviceEngineEmailNotifReport(Model model) {
+        model.addAttribute("resultList",csvReadWriteUtils.readCsvToListMap(seAutoEmailLog));
         return "serviceEngineEmailNotifReport";
     }
 
     @RequestMapping(value = "/dcemailnotifreport", method = RequestMethod.GET)
     public String dataConnectorEmailNotifReport(Model model) {
+        model.addAttribute("resultList",csvReadWriteUtils.readCsvToListMap(dcAutoEmailLog));
         return "dataConnectorEmailNotifReport";
     }
 
     @RequestMapping(value = "/bmemailnotifreport", method = RequestMethod.GET)
     public String batchManagerEmailNotifReport(Model model) {
+        model.addAttribute("resultList",csvReadWriteUtils.readCsvToListMap(bmAutoEmailLog));
         return "batchManagerEmailNotifReport";
     }
 
     @GetMapping("/adhocemailnotifreport")
     public String adhocEmailNotifReport(Model model) {
+        model.addAttribute("resultList",csvReadWriteUtils.readCsvToListMap(adhocEmailLog));
         return "adhocEmailNotifReport";
     }
 

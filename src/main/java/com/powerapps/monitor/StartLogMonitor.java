8f9fa82ddafb5b4f78c1dfefe526a4cfb64ce1e6
Josh Uzo <nwankwo.joshua@gmail.com>
2018-08-27 15:17:28 +0800

    Cleanup unused imports

--- src/main/java/com/powerapps/monitor/StartLogMonitor.java
+++ src/main/java/com/powerapps/monitor/StartLogMonitor.java
@@ -1,16 +1,8 @@
 package com.powerapps.monitor;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import com.powerapps.monitor.config.EmailConfigAssembler;
-
-
 
 @SpringBootApplication
 @EnableScheduling

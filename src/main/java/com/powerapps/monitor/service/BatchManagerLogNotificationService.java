bdea8ff6f0c54e7da8f3f96bce6d3f427b842e8e
joshua <nwankwo.joshua@gmail.com>
2019-02-07 10:17:45 +1100

    Re-indented 4 spaces

--- src/main/java/com/powerapps/monitor/service/BatchManagerLogNotificationService.java
+++ src/main/java/com/powerapps/monitor/service/BatchManagerLogNotificationService.java
@@ -23,94 +23,94 @@ import com.powerapps.monitor.model.LogSummary;
 @Service
 public class BatchManagerLogNotificationService {
 
-  private static final Logger LOG = LoggerFactory.getLogger(BatchManagerLogNotificationService.class);
-  public final BatchManagerLogService logService;
-  private final IEmailContentBuilder emailContentBuilder;
-  private final IEmailClient emailClient;
-  private final EmailConfigEntity emailConfig;
+    private static final Logger LOG = LoggerFactory.getLogger(BatchManagerLogNotificationService.class);
+    private final BatchManagerLogService logService;
+    private final IEmailContentBuilder emailContentBuilder;
+    private final IEmailClient emailClient;
+    private final EmailConfigEntity emailConfig;
 
-  @Value("${app.emailMaxQueueSize}")
-  String emailMaxQueueSize;
+    @Value("${app.emailMaxQueueSize}")
+    private String emailMaxQueueSize;
 
-  @Value("${app.bm.emailTitle}")
-  String emailTitle;
+    @Value("${app.bm.emailTitle}")
+    private String emailTitle;
 
-  @Value("${app.mbsb.bm.itoperator}")
-  String itoperator;
+    @Value("${app.mbsb.bm.itoperator}")
+    private String itoperator;
 
-  // emailMaxQueueSize
-  @Autowired
-  public BatchManagerLogNotificationService(BatchManagerLogService logService, IEmailContentBuilder emailContentBuilder,
-                                            IEmailClient emailClient, EmailConfigEntity emailConfig) {
-    this.logService = logService;
-    this.emailContentBuilder = emailContentBuilder;
-    this.emailClient = emailClient;
-    this.emailConfig = emailConfig;
-  }
-
-  public void execute(String recipient) throws IOException {
-    final String recipientTmp = recipient;
-    List<LogSummary> summaries = logService.getBmLogSummaries();
-    BmProperties bmConfig = logService.bmConfig;
-
-    /* Build email content */
-    String emailContent = null;
-    String title = null;
-
-    int queueMaxSize = Integer.parseInt(emailMaxQueueSize);
-    LOG.info("Processing {} of {} total Batch Manager logs", queueMaxSize, summaries.size());
-    LOG.info("Queue max size: {}", queueMaxSize);
-
-    if (summaries.size() > 0) {
-      for (int i = 0; i < queueMaxSize; i++) {
-        LogSummary summary = summaries.get(i);
-        int batchStatus = summary.getBatchStatus();
-        String batchLogName = summary.getLogFileName();
-        LOG.info("Processing: {}", batchLogName);
-        File file = new File(bmConfig.getBmRootPath() + "/", batchLogName);
-        emailContent = emailContentBuilder.buildEmailTemplate("fragments/template_bm_email", summary);
-
-        if (batchStatus != 3) {
-          if (batchStatus == 1) {
-            /* failed email title */
-            title = MessageFormat.format("{0}: {1} {2}", new Object[]{"FAILED", emailTitle, batchLogName});
-
-          } else {
-            /* successful email title */
-            title = MessageFormat.format("{0}: {1} {2}", new Object[]{"SUCCESSFUL", emailTitle, batchLogName});
-
-            if (title.startsWith("SUCCESSFUL: MBSB Completion of PowerKollect Daily Batch loading for  Batch-daily-")) {
-              recipient = itoperator;
-            } else {
-              recipient = recipientTmp;
-            }
-          }
-          /* Construct and assemble email object */
-          Email mail = new EmailBuilder()
-                  .from(emailConfig.getFromEmail())
-                  .to(recipient)
-                  .body(emailContent)
-                  .title(title)
-                  .includeFileAtt(file)
-                  .includeAtt(null)
-                  .create();
-
-          /* Send email */
-          String emailStatus = emailClient.execute(mail);
-
-          /* if email was sent then persist to cache */
-          persistToCache(emailStatus, bmConfig.getBmCache(), batchLogName);
-
-        }
-      }
+    // emailMaxQueueSize
+    @Autowired
+    public BatchManagerLogNotificationService(BatchManagerLogService logService, IEmailContentBuilder emailContentBuilder,
+                                              IEmailClient emailClient, EmailConfigEntity emailConfig) {
+        this.logService = logService;
+        this.emailContentBuilder = emailContentBuilder;
+        this.emailClient = emailClient;
+        this.emailConfig = emailConfig;
     }
 
-  }
+    public void execute(String recipient) throws IOException {
+        final String recipientTmp = recipient;
+        List<LogSummary> summaries = logService.getBmLogSummaries();
+        BmProperties bmConfig = logService.bmConfig;
+
+        /* Build email content */
+        String emailContent = null;
+        String title = null;
+
+        int queueMaxSize = Integer.parseInt(emailMaxQueueSize);
+        LOG.info("Processing {} of {} total Batch Manager logs", queueMaxSize, summaries.size());
+        LOG.info("Queue max size: {}", queueMaxSize);
+
+        if (summaries.size() > 0) {
+            for (int i = 0; i < queueMaxSize; i++) {
+                LogSummary summary = summaries.get(i);
+                int batchStatus = summary.getBatchStatus();
+                String batchLogName = summary.getLogFileName();
+                LOG.info("Processing: {}", batchLogName);
+                File file = new File(bmConfig.getBmRootPath() + "/", batchLogName);
+                emailContent = emailContentBuilder.buildEmailTemplate("fragments/template_bm_email", summary);
+
+                if (batchStatus != 3) {
+                    if (batchStatus == 1) {
+                        /* failed email title */
+                        title = MessageFormat.format("{0}: {1} {2}", new Object[]{"FAILED", emailTitle, batchLogName});
+
+                    } else {
+                        /* successful email title */
+                        title = MessageFormat.format("{0}: {1} {2}", new Object[]{"SUCCESSFUL", emailTitle, batchLogName});
+
+                        if (title.startsWith("SUCCESSFUL: MBSB Completion of PowerKollect Daily Batch loading for  Batch-daily-")) {
+                            recipient = itoperator;
+                        } else {
+                            recipient = recipientTmp;
+                        }
+                    }
+                    /* Construct and assemble email object */
+                    Email mail = new EmailBuilder()
+                            .from(emailConfig.getFromEmail())
+                            .to(recipient)
+                            .body(emailContent)
+                            .title(title)
+                            .includeFileAtt(file)
+                            .includeAtt(null)
+                            .create();
+
+                    /* Send email */
+                    String emailStatus = emailClient.execute(mail);
+
+                    /* if email was sent then persist to cache */
+                    persistToCache(emailStatus, bmConfig.getBmCache(), batchLogName);
+
+                }
+            }
+        }
+
+    }
 
 
-  private void persistToCache(String emailStatus, String cacheFilePath, String batchLogName) {
-    if (emailStatus.equals("Success"))
-      new FileUtils().writeTextFile(cacheFilePath, batchLogName + "\n");
-  }
+    private void persistToCache(String emailStatus, String cacheFilePath, String batchLogName) {
+        if (emailStatus.equals("Success"))
+            new FileUtils().writeTextFile(cacheFilePath, batchLogName + "\n");
+    }
 
 }
